a5aa353c02aab7deacc3ef7c6735440c89a8ea21
daniel.lufupa <daniel.lufupa@dasa.com.br>
2022-09-20 23:24:22 -0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import React from "react";
-import Api from "./api/Api.js";
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Components/Home.js'
+import Header from "./Components/Header.js";
+import Footer from "./Components/Footer.js"
+import Login from "./Components/Login/Login";
+
 
 
 function App() {
   return (
-   <Api></Api>
+   <div>
+    <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path={'/'} element={<Home/>} />
+      <Route path={'/Login'} element={<Login/>} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+   </div>
   )
 }
 
